fe7d7e1e739eda677c37a855c4f942eb03e94795
jbuehlz <jonathanbuehler@gmail.com>
2022-07-13 17:29:46 -0500

    queue_api: enable joining queue and checking queue position

--- queue_api/queue_api/api/public/queue/join.py
+++ queue_api/queue_api/api/public/queue/join.py
@@ -1,7 +1,11 @@
 from hashlib import md5
-
 from flask import request
 from flask_restful import Resource
+
+from queue_api.queue_api.db.models.account import Account
+from queue_api.queue_api.db.models.queue import QueueEntries
+from queue_api.queue_api.db.models.whitelist import WhitelistEntries
+from queue_api.queue_api.db.database import db
 
 
 class QueueJoin(Resource):
@@ -13,15 +17,16 @@ class QueueJoin(Resource):
             hashed_password = "##" + md5(password).hexdigest()
             account = Account.query.filter_by(name=name, password=hashed_password).first()
             if account is None:
-                return jsonify(
-                    {'success': False, 'error': 'an account with this user/pass combination does not exist'}), 401
+                return {'success': False, 'error': 'an account with this user/pass combination does not exist'}, 401
             if account.privlevel > 1:
-                return jsonify({'success': True, 'queue_bypass': True})
-            if account.name in queue:
-                return jsonify({'success': False, 'position': queue.index(account.name) + 1})
-            queue.append(account.name)
-            return jsonify({'success': True, 'position': len(queue)})
+                return {'success': True, 'queue_bypass': True}, 200
+            if WhitelistEntries.query.filter_by(name=account.name).first():
+                return {'success': False, 'whitelisted': True}, 304
+            if QueueEntries.query.filter_by(name=account.name).first():
+                return {'success': False, 'queued': True}, 304
+            queue_ticket = QueueEntries(name=account.name)
+            db.session.add(queue_ticket)
+            db.session.commit()
         except KeyError as e:
-            return jsonify({'success': False, 'error': str(e)}), 400
-
-        return
+            return {'success': False, 'error': str(e)}, 400
+        return {'success': True, 'queued': True}, 200
