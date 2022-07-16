06c27ee8ec94eaa7c9fb296654109c6251771479
jbuehlz <jonathanbuehler@gmail.com>
2022-07-16 00:24:42 -0500

    queue_api: app: change DB_URLs and local port

--- queue_api/queue_api/app.py
+++ queue_api/queue_api/app.py
@@ -2,12 +2,12 @@ from flask import Flask
 
 from .api import v1
 
-from queue_api.queue_api.db.database import DATABASE_URL, db
+from queue_api.queue_api.db.database import QUEUE_DATABASE_URL, db
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_DATABASE_URI"] = QUEUE_DATABASE_URL
     app.config["SQLALCHEMY_ECHO"] = False
     db.init_app(app)
     app.register_blueprint(v1, url_prefix="/api/v1")
@@ -16,6 +16,6 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
 else:
     app = create_app()
