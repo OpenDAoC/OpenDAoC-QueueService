f526693222f8dc1fb5cb4eebe67acf3f2a464e1d
jbuehlz <jonathanbuehler@gmail.com>
2022-07-16 00:24:18 -0500

    queue_api: internal: finished

--- queue_api/queue_api/api/internal/queue.py
+++ queue_api/queue_api/api/internal/queue.py
@@ -1,18 +1,24 @@
+import traceback
+
+from flask import request
 from flask_restful import Resource
+from sqlalchemy import text
 
 from .security_decorator import internal_security
+from ...db.database import db
 
 
 class InternalQueue(Resource):
+    decorators = [internal_security]
 
-    @internal_security
-    def get(self):
-        return
-
-    @internal_security
     def post(self):
-        return
-
-    @internal_security
-    def delete(self):
-        return
+        data = request.get_json()
+        try:
+            length = data["length"]
+            payload = {"length": length}
+            query = text(
+                """SELECT name FROM queue_entries WHERE whitelisted IS NOT TRUE ORDER BY date_create LIMIT :length""")
+            res = db.session.execute(query, payload)
+        except Exception as e:
+            return {"success": False, "error": str(traceback.format_exc())}
+        return [dict(ix) for ix in res], 200
