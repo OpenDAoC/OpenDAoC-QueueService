41081cbae50732eeeda195724f668f93aa80b113
jbuehlz <jonathanbuehler@gmail.com>
2022-07-16 00:24:11 -0500

    queue_api: public: finished

--- queue_api/queue_api/api/public/queue/join.py
+++ queue_api/queue_api/api/public/queue/join.py
@@ -1,41 +1,59 @@
+import json
 from hashlib import md5
 from flask import request
 from flask_restful import Resource
 
-# from queue_api.queue_api.db.models.account import Account
 from sqlalchemy import text
 
-from queue_api.queue_api.db.atlas_database import atlas_session
+from queue_api.queue_api.api.public.queue.queue import get_queue_position_by_name
+from queue_api.queue_api.db.atlas_database import atlas_session, ATLAS_DATABASE_URL
 from queue_api.queue_api.db.models.queue import QueueEntries
-from queue_api.queue_api.db.models.whitelist import WhitelistEntries
-from queue_api.queue_api.db.database import db
+from queue_api.queue_api.db.database import db, QUEUE_DATABASE_URL
 
 
 class QueueJoin(Resource):
+
+    # JOIN QUEUE
     def post(self):
         data = request.get_json()
         try:
             name = data['name']
-            password = data['password'].encode('utf-8')
-            hashed_password = "##" + md5(password).hexdigest()
+            password = data['password']
+
+            res = []
+            for i in range(0, len(password)):
+                res += chr(ord(password[i]) >> 8)
+                res += chr(ord(password[i]))
+            encoded_password_string = "".join(res).encode('utf-8')
+            hashed = md5(encoded_password_string).hexdigest().upper()
+            hash_len = len(hashed)
+            for x in (reversed(range(0, hash_len - 1, 2))):
+                if hashed[x] == "0":
+                    hashed = hashed[0:x] + hashed[x+1:hash_len]
+
+            hashed_password = "##" + hashed
             game_server_session = atlas_session()
             payload = {"name": name, "password": hashed_password}
-            query = text("""SELECT COUNT(1) FROM account WHERE name=:name AND password=:password""")
-            account = game_server_session.query(query, **payload)
-            print(account, flush=True)
-            return account, 200
+            query = text("""SELECT name, privlevel FROM account WHERE name=:name AND password=:password""")
+            res = game_server_session.execute(query, payload)
+            account = res.first()
+
             # account = Account.query.filter_by(name=name, password=hashed_password).first()
             if account is None:
                 return {'success': False, 'error': 'an account with this user/pass combination does not exist'}, 401
             if account.privlevel > 1:
-                return {'success': True, 'queue_bypass': True}, 200
-            if WhitelistEntries.query.filter_by(name=account.name).first():
-                return {'success': False, 'whitelisted': True}, 304
-            if QueueEntries.query.filter_by(name=account.name).first():
-                return {'success': False, 'queued': True}, 304
+                return {'success': True, 'queue_bypass': True, 'queued': False, 'whitelisted': False}, 200
+            if QueueEntries.query.filter_by(name=account.name, whitelisted=True).first():
+                return {'success': True, 'queue_bypass': False, 'queued': False, 'whitelisted': True}, 200
+            if QueueEntries.query.filter_by(name=account.name, whitelisted=False).first():
+                return {'success': True, 'queue_bypass': False, 'queued': True, 'whitelisted': False}, 200
             queue_ticket = QueueEntries(name=account.name)
             db.session.add(queue_ticket)
             db.session.commit()
-        except KeyError as e:
-            return {'success': False, 'error': str(e)}, 400
-        return {'success': True, 'queued': True}, 200
+
+            position, status = get_queue_position_by_name(name)
+            if status != 200:
+                return position, status
+        except Exception as e:
+            return {'success': False, 'error': str(e), "queue_db_uri": QUEUE_DATABASE_URL, "atlas_db_uri": ATLAS_DATABASE_URL}, 400
+        return {'success': True, 'queued': True, 'position': position}, 200
--- queue_api/queue_api/api/public/queue/queue.py
+++ queue_api/queue_api/api/public/queue/queue.py
@@ -1,10 +1,10 @@
 import traceback
 
-from flask import request, jsonify
+from flask import request
 from flask_restful import Resource
 from sqlalchemy import text
 
-from queue_api.queue_api.db.database import db, DATABASE_URL
+from queue_api.queue_api.db.database import db
 from queue_api.queue_api.db.models.queue import QueueEntries
 
 
@@ -12,16 +12,25 @@ class PublicQueue(Resource):
     def get(self):
         return {'success': True, 'queue_length': 0}, 200
 
+    # GET QUEUE POSITION
     def post(self):
         data = request.get_json()
         try:
-            account = data["account"]
-            queue_ticket = QueueEntries.query.filter_by(name=account).first()
-            if not queue_ticket:
-                return {'success': False, 'message': 'account is not in queue', 'queued': False}, 400
-            payload = {"create_data": queue_ticket.date_create}
-            query = text("""SELECT COUNT(1) FROM tbl WHERE create_date <= :create_date""")
-            result = db.engine.execute(query, **payload)
+            name = data["name"]
+            position, status = get_queue_position_by_name(name)
+            if status != 200:
+                return position, status
         except Exception as e:
-            return {'success': False, 'error': str(traceback.format_exc()), 'db_uri': DATABASE_URL}, 500
-        return {'success': True, 'position': result}, 200
+            return {'success': False, 'position': -1, 'error': str(traceback.format_exc())}, 500
+        return {'success': True, 'position': position}, 200
+
+
+def get_queue_position_by_name(name):
+    queue_ticket = QueueEntries.query.filter_by(name=name).first()
+    if not queue_ticket:
+        return {'success': False, 'error': 'user is not in queue', 'position': -1}, 404
+    payload = {"date_create": queue_ticket.date_create.isoformat()}
+    query = text("""SELECT COUNT(1) FROM queue_entries WHERE whitelisted IS NOT TRUE AND date_create <= :date_create""")
+    result = db.engine.execute(query, **payload).first()
+    position = result[0]
+    return position, 200
